481d9e41f5f079ab99ddecfd22c47d572c4b3835
Shivalika Phatak <shivalikap@synerzipune.local>
2016-09-13 16:20:20 +0530

    New Files

---
+++ XMLReader.java
@@ -0,0 +1,46 @@
+package Snapshot.AWSTest;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class XMLReader {
+	
+	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+		
+		File fXmlFile = new File("TestXMLFile.xml");
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		Document doc = dBuilder.parse(fXmlFile);
+		
+		doc.getDocumentElement().normalize();
+		System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+		NodeList nList = doc.getElementsByTagName("staff");
+		System.out.println("----------------------------");
+		
+		for (int temp = 0; temp < nList.getLength(); temp++) {
+			Node nNode = nList.item(temp);
+			System.out.println("\nCurrent Element :" + nNode.getNodeName());
+			
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+				Element eElement = (Element) nNode;
+
+				System.out.println("Staff id : " + eElement.getAttribute("id"));
+				System.out.println("First Name : " + eElement.getElementsByTagName("firstname").item(0).getTextContent());
+				System.out.println("Last Name : " + eElement.getElementsByTagName("lastname").item(0).getTextContent());
+				System.out.println("Nick Name : " + eElement.getElementsByTagName("nickname").item(0).getTextContent());
+				System.out.println("Salary : " + eElement.getElementsByTagName("salary").item(0).getTextContent());
+			}
+		}
+	}
+
+}
