028478a69a1f0bbb25ad0895a83243efe68723eb
Shivalika Phatak <shivalikap@synerzipune.local>
2016-09-19 17:00:14 +0530

    IReporter

---
+++ SampleTest.java
@@ -0,0 +1,23 @@
+package Snapshot.AWSTest;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	
+	@Test
+    public void testMethodOne(){
+        Assert.assertTrue(true);
+    }
+	  
+    @Test
+    public void testMethodTwo(){
+	Assert.assertTrue(false);
+    }
+	  
+    @Test(dependsOnMethods={"testMethodTwo"})
+        public void testMethodThree(){
+        Assert.assertTrue(true);
+    }
+
+}
